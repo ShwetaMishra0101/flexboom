c99246e255b90b2b04dedf20b2a24d11bb9b292b
Shweta Mishra <shweta.aith@gmail.com>
2023-07-06 16:44:25 +0530

    add home page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+//= require jquery3
+//= require popper
+//= require bootstrap
+import 'bootstrap'
+import 'bootstrap/dist/js/bootstrap'
+document.addEventListener("DOMContentLoaded", function() {
+    const accordionHeaders = document.querySelectorAll(".accordion-header");
+    
+    accordionHeaders.forEach(function(header) {
+      header.addEventListener("click", function() {
+        // Toggle the active class on the clicked header
+        this.classList.toggle("active");
+        
+        // Toggle the display of the accordion content
+        const accordionContent = this.nextElementSibling;
+        accordionContent.style.display = accordionContent.style.display === "block" ? "none" : "block";
+      });
+    });
+  });
